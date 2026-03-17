44c42dc9f60ef1d6754facb2dcf00ed731ac05a4
Ashaverus <kaykymdlima@gmail.com>
2026-03-17 11:43:37 -0300

    Tentando a Cobra

--- tasks/ed/gomos/go/draft.go
+++ tasks/ed/gomos/go/draft.go
@@ -1,5 +1,35 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var N int
+	fmt.Scan(&N)
+	x := make([]int, N)
+	y := make([]int, N)
+	var D string
+	fmt.Scan(&D)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&x[i], &y[i])
+	}
+
+	for i := 0; i < N; i++ {
+		if i == 0 {
+			continue
+		}
+		x[i] = x[i-1]
+		y[i] = y[i-1]
+	}
+	if D == "R" {
+		x[0] = x[0] + 1
+	} else if D == "L" {
+		x[0] = x[0] - 1
+	} else if D == "U" {
+		y[0] = y[0] + 1
+	} else if D == "D" {
+		y[0] = y[0] - 1
+	}
+	for i := 0; i < N; i++ {
+		fmt.Printf("%d %d\n", x[i], y[i])
+	}
 }
