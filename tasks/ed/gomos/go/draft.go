0c16170b6752bc22e8f3a2f5feca449ed7eab8a3
Ashaverus <kaykymdlima@gmail.com>
2026-03-18 20:22:01 -0300

    Gnomooossss

--- tasks/ed/gomos/go/draft.go
+++ tasks/ed/gomos/go/draft.go
@@ -13,10 +13,7 @@ func main() {
 		fmt.Scan(&x[i], &y[i])
 	}
 
-	for i := 0; i < N; i++ {
-		if i == 0 {
-			continue
-		}
+	for i := N - 1; i > 0; i-- {
 		x[i] = x[i-1]
 		y[i] = y[i-1]
 	}
@@ -25,9 +22,9 @@ func main() {
 	} else if D == "L" {
 		x[0] = x[0] - 1
 	} else if D == "U" {
-		y[0] = y[0] + 1
-	} else if D == "D" {
 		y[0] = y[0] - 1
+	} else if D == "D" {
+		y[0] = y[0] + 1
 	}
 	for i := 0; i < N; i++ {
 		fmt.Printf("%d %d\n", x[i], y[i])
