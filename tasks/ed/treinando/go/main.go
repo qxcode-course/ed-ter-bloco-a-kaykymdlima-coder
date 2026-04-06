ddb00e90a0bf9ab4a39ec3e10b45aed5b2a8a623
Ashaverus <kaykymdlima@gmail.com>
2026-04-06 20:25:17 -0300

    treinando

--- tasks/ed/treinando/go/main.go
+++ tasks/ed/treinando/go/main.go
@@ -9,30 +9,67 @@ import (
 )
 
 func tostr(vet []int) string {
-	_ = vet
-	return ""
+	if len(vet) == 0 {
+		return "[]"
+	}
+	return "[" + tostrrecu(vet) + "]"
+}
+func tostrrecu(vet []int) string {
+	//ultimo caso
+	if len(vet) == 1 {
+		return fmt.Sprintf("%d", vet[0])
+	}
+	//outros casos
+	return fmt.Sprintf("%d, ", vet[0]) + tostrrecu(vet[1:])
 }
 
 func tostrrev(vet []int) string {
-	_ = vet
-	return ""
+	if len(vet) == 0 {
+		return "[]"
+	}
+	return "[" + tostrrevrecu(vet) + "]"
+}
+func tostrrevrecu(vet []int) string {
+	if len(vet) == 1 {
+		return fmt.Sprintf("%d", vet[0])
+	}
+	//msm coisa, soq pega o id do ultimo caso
+	ultid := len(vet) - 1
+	return fmt.Sprintf("%d, ", vet[ultid]) + tostrrevrecu(vet[:ultid])
 }
 
 // reverse: inverte os elementos do slice
 func reverse(vet []int) {
-	_ = vet
+	//quando o espaçamento acaba, o vetor tbm
+	if len(vet) <= 1 {
+		return
+	}
+	//troca as pontas
+	vetori := vet[0]
+	vet[0] = vet[len(vet)-1]
+	vet[len(vet)-1] = vetori
+	///reduz o espacamento do vetor a cada giro
+	reverse(vet[1 : len(vet)-1])
 }
 
 // sum: soma dos elementos do slice
 func sum(vet []int) int {
-	_ = vet
-	return 0
+	//se 0, 0
+	if len(vet) == 0 {
+		return 0
+	}
+	//faz a soma recu
+	return vet[0] + sum(vet[1:])
 }
 
 // mult: produto dos elementos do slice
 func mult(vet []int) int {
-	_ = vet
-	return 0
+	//se 0, 0
+	if len(vet) == 0 {
+		return 1
+	}
+	//faz a msm coisa da soma recu soq *
+	return vet[0] * mult(vet[1:])
 }
 
 // min: retorna o índice e valor do menor valor
@@ -40,8 +77,30 @@ func mult(vet []int) int {
 // var rec func(v []int) (int, int)
 // para fazer uma recursão que retorna valor e índice
 func min(vet []int) int {
-	_ = vet
-	return 0
+	// if 0 -1
+	if len(vet) == 0 {
+		return -1
+	}
+	var rec func(v []int) (int, int)
+	//recursiva interna
+	rec = func(v []int) (int, int) {
+		// se so 1
+		if len(v) == 1 {
+			return 0, v[0]
+		}
+		//recursiva
+		idres, valres := rec(v[1:])
+		idslide := idres + 1
+
+		///compara
+		if v[0] <= valres {
+			return 0, v[0]
+		} else {
+			return idslide, valres
+		}
+	}
+	indiceFinal, _ := rec(vet)
+	return indiceFinal
 }
 
 func main() {
