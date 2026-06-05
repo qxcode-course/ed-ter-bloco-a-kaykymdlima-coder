d3f6aa91ff3da0568dcfed46676b4ae6f7edb95a
Ashaverus <kaykymdlima@gmail.com>
2026-06-05 20:51:08 -0300

    Permutando coisas

--- tasks/ed/permutacoes/src/go/draft.go
+++ tasks/ed/permutacoes/src/go/draft.go
@@ -1,5 +1,53 @@
 package main
+
 import "fmt"
+
+func ordem(letras []rune) {
+	l := len(letras)
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-i-1; j++ {
+			if letras[j] > letras[j+1] {
+				letras[j], letras[j+1] = letras[j+1], letras[j]
+			}
+		}
+	}
+}
+func permutacao(p string) []string {
+	letras := []rune(p)
+	ordem(letras)
+	var resu []string
+	lidas := make([]bool, len(letras))
+	var atual []rune
+
+	backtrack(letras, lidas, atual, &resu)
+
+	return resu
+}
+
+func backtrack(letras []rune, lidas []bool, atual []rune, resu *[]string) {
+	if len(atual) == len(letras) {
+		*resu = append(*resu, string(atual))
+		return
+	}
+
+	for i, _ := range letras {
+		if lidas[i] {
+			continue
+		}
+		lidas[i] = true
+		atual = append(atual, letras[i])
+		backtrack(letras, lidas, atual, resu)
+		lidas[i] = false
+		atual = atual[:len(atual)-1]
+	}
+
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var entrada string
+	fmt.Scanln(&entrada)
+	permutacoes := permutacao(entrada)
+	for _, saida := range permutacoes {
+		fmt.Println(saida)
+	}
 }
