944cf3df70f61cd4413add482f1a495a8f79bf67
Ashaverus <kaykymdlima@gmail.com>
2026-07-04 13:42:08 -0300

    Nenhuma...

---
+++ tasks/ed/escapeSpread/src/go/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Não modifique a assinatura da função numIslands
+// Ela é a função que será chamada no LeetCode para resolver o problema
+const inf = 1e9
+
+func numIslands(grid [][]byte) int {
+}
+
+// Não modifique a função main
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	var nl, nc int
+	fmt.Sscanf(line, "%d %d", &nl, &nc)
+	grid := make([][]byte, nl)
+	for i := 0; i < nl; i++ {
+		scanner.Scan()
+		grid[i] = []byte(scanner.Text())
+	}
+	result := numIslands(grid)
+	fmt.Println(result)
+}
