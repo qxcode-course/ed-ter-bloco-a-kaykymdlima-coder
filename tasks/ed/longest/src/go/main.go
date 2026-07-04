20f19d29c4248f91f8a1b1a3c94de0c0b4e61b5c
Ashaverus <kaykymdlima@gmail.com>
2026-07-04 11:34:24 -0300

    longest

--- tasks/ed/longest/src/go/main.go
+++ tasks/ed/longest/src/go/main.go
@@ -9,9 +9,47 @@ import (
 )
 
 func longestIncreasingPath(matrix [][]int) int {
-	//
-	_ := matrix
-	return 0
+	rs := len(matrix)
+	cs := len(matrix[0])
+	me := make([][]int, rs)
+	for i := range me {
+		me[i] = make([]int, cs)
+	}
+
+	pat := [][]int{
+		{1, 0},
+		{-1, 0},
+		{0, 1},
+		{0, -1}}
+
+	var busca func(r, c int) int
+	busca = func(r, c int) int {
+		if me[r][c] != 0 {
+			return me[r][c]
+		}
+		maxpat := 1
+		for _, d := range pat {
+			ner, nec := r+d[0], c+d[1]
+			if ner >= 0 && ner < rs && nec >= 0 && nec < cs && matrix[ner][nec] > matrix[r][c] {
+				path := 1 + busca(ner, nec)
+				if path > maxpat {
+					maxpat = path
+				}
+			}
+		}
+		me[r][c] = maxpat
+		return maxpat
+	}
+	long := 0
+	for r := 0; r < rs; r++ {
+		for c := 0; c < cs; c++ {
+			patl := busca(r, c)
+			if patl > long {
+				long = patl
+			}
+		}
+	}
+	return long
 }
 
 // Não modifique a função main
