6d9ec130531682497f648c1db24f6281caef4046
Ashaverus <kaykymdlima@gmail.com>
2026-07-04 10:42:18 -0300

    cercado

--- tasks/ed/cercado/src/go/main.go
+++ tasks/ed/cercado/src/go/main.go
@@ -8,7 +8,35 @@ import (
 
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	rs := len(board)
+	cs := len(board[0])
+	visto := make([][]bool, rs)
+	for i := range visto {
+		visto[i] = make([]bool, cs)
+	}
+	var busca func(r, c int)
+	busca = func(r, c int) {
+		if r < 0 || r >= rs || c < 0 || c >= cs || board[r][c] == 'X' || visto[r][c] {
+			return
+		}
+		visto[r][c] = true
+		busca(r+1, c)
+		busca(r-1, c)
+		busca(r, c+1)
+		busca(r, c-1)
+	}
+	for c := 0; c < cs; c++ {
+		busca(0, c)
+		busca(rs-1, c)
+	}
+	for r := 0; r < rs; r++ {
+		for c := 0; c < cs; c++ {
+			if board[r][c] == 'O' && !visto[r][c] {
+				board[r][c] = 'X'
+			}
+		}
+	}
+
 }
 
 // NÃO ALTERE A MAIN
